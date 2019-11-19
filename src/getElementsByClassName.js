9e5e233867ab7e9831134dccc84f751b1c23fb75
AdamMomen <adammuman81@gmail.com>
2019-11-19 20:58:28 +0100

    Test v1.0

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,10 +1,33 @@
 // If life was easy, we could just do things the easy way:
-// var getElementsByClassName = function (className) {
-//   return document.getElementsByClassName(className);
-// };
+var getElementsByClassName = function (className) {
+  return document.getElementsByClassName(className);
+ };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+	debugger;
+	var i = 0;
+	var array = [];
+	var body = function(i) {
+		array.push(document.body.childNodes)
+		if(array[i].classList === className) {
+			return true;
+		}
+		return body(i + 1);
+	}
+	return body(i)
 };
+
+
+var getElementsByClassName = function(className, i, j) {
+	var j = j ||0;
+	var i = i|| 0;
+	//var array = [];
+		if(document.body.childNodes[i].classList[j] === className) {
+			return document.body.childNodes[i].classList[j];
+		}
+		if(j <  document.body.childNodes[i].classList) {
+			return getElementsByClassName(className, i , j + 1);
+		}
+		return getElementsByClassName(className, i+ 1 ,j);
+	};
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,8 +1,13 @@
 // this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
+ var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+	var keys = Object.keys(obj)[0];
+	var values = Object.values(obj)[0];
+	delete obj.keys;
+	var str = '';
+	console.log(obj);
+	return str += keys + values
 };
